bde02e530c8c4c413505844b289bd1a885201a89
Paul M. Hutchinson <phutchinson@labelinsight.com>
2015-10-06 12:08:03 -0500

    Update solrcloud_install.rb
    Added creation of core directories and .properties files

--- opsworks_solrcloud/recipes/solrcloud_install.rb
+++ opsworks_solrcloud/recipes/solrcloud_install.rb
@@ -151,3 +151,42 @@ end
 remote_file tarball_file do
   action :delete
 end
+
+directory '/usr/local/solr/solr/cores/core1' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/usr/local/solr/solr/cores/core2' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/usr/local/solr/solr/cores/core3' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+file '/usr/local/solr/solr/cores/core1/core.properties' do
+  content 'name=core1'
+  mode '0755'
+  owner 'ec2-user'
+end
+
+file '/usr/local/solr/solr/cores/core2/core.properties' do
+  content 'name=core2'
+  mode '0755'
+  owner 'ec2-user'
+end
+
+file '/usr/local/solr/solr/cores/core3/core.properties' do
+  content 'name=core3'
+  mode '0755'
+  owner 'ec2-user'
+end
