7ca13dc6d11908328df8680ef4d81955534e742a
Paul M. Hutchinson <phutchinson@labelinsight.com>
2015-10-09 09:08:59 -0500

    Update solrcloud_deployconfig.rb
    added block that creates collections dynamically

--- opsworks_solrcloud/recipes/solrcloud_deployconfig.rb
+++ opsworks_solrcloud/recipes/solrcloud_deployconfig.rb
@@ -51,3 +51,28 @@ node['solrcloud']['collections'].each do |collection_name, options|
     action options[:action]
   end
 end
+
+config_directory = '/usr/local/solr_zkconfigsets'
+collections = Dir.entries(config_directory).select {|entry| File.directory? File.join(config_directory,entry) and !(entry =='.' || entry == '..' || entry.start_with?('.')) }
+
+execute '/usr/local/solr_zkconfigsets' do
+    unless collections.empty?
+      collections.each do |collection|
+        unless collection.to_s.nil? or collection.to_s.empty? or collection.to_s.blank?
+          # create current collection
+          command "/usr/local/solr-5.3.0/bin/./solr create -c #{collection.to_s}"
+          # store 'managed-schema' for current collection
+          #file "/etc/init.d/someService" do
+          #  owner 'solr'
+          #  group 'solr'
+          #  mode 0755
+          #  content ::File.open("/usr/local/solr_zkconfigsets/#{collection}/conf/managed-schema").read
+          #  action :create
+          #end
+          # store 'solrconfig.xml' for current collection
+          
+        end
+      end
+    end
+    ignore_failure true
+end
