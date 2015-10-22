8708287c2e539d1df952f6ddccd229a72a784a0d
Paul M. Hutchinson <phutchinson@labelinsight.com>
2015-10-22 13:10:03 -0500

    Update deploy_custom_config.rb
    Chef got angry at my log statements

--- opsworks_solrcloud/recipes/deploy_custom_config.rb
+++ opsworks_solrcloud/recipes/deploy_custom_config.rb
@@ -1,17 +1,17 @@
 action :getconfig do
   run_context.include_recipe 'aws'
-  Chef::Log.info("Beginning deploy_custom_config recipe...")
-  Chef::Log.info('Getting solr configuration from s3 bucket')
+  #Chef::Log.info("Beginning deploy_custom_config recipe...")
+  #Chef::Log.info('Getting solr configuration from s3 bucket')
 
   zkconfigtar_tmp = '/tmp/zkconfigtar/'
   
-  Chef::Log.info("Deleting directory: #{zkconfigtar_tmp}")
+  #Chef::Log.info("Deleting directory: #{zkconfigtar_tmp}")
   directory zkconfigtar_tmp do
     recursive true
     action :delete
   end
 
-  Chef::Log.info("Creating directory: #{zkconfigtar_tmp}")
+  #Chef::Log.info("Creating directory: #{zkconfigtar_tmp}")
   directory zkconfigtar_tmp do
     owner 'root'
     group 'root'
@@ -19,18 +19,18 @@ action :getconfig do
     action :create
   end
 
-  Chef::Log.info("Retrieving file from s3: #{zkconfigtar_tmp}solrconfig.tar.gz")
+  #Chef::Log.info("Retrieving file from s3: #{zkconfigtar_tmp}solrconfig.tar.gz")
   aws_s3_file "#{zkconfigtar_tmp}solrconfig.tar.gz" do
-    Chef::Log.info("Bucket: #{zkconfigsets_s3_bucket}")
+    #Chef::Log.info("Bucket: #{zkconfigsets_s3_bucket}")
     bucket new_resource.zkconfigsets_s3_bucket
     
-    Chef::Log.info("Remote path: #{zkconfigsets_s3_remote_path}")
+    #Chef::Log.info("Remote path: #{zkconfigsets_s3_remote_path}")
     remote_path new_resource.zkconfigsets_s3_remote_path
     
-    Chef::Log.info("AWS Access Key ID: #{zkconfigsets_s3_aws_access_key_id}")
+    #Chef::Log.info("AWS Access Key ID: #{zkconfigsets_s3_aws_access_key_id}")
     aws_access_key_id new_resource.zkconfigsets_s3_aws_access_key_id
     
-    Chef::Log.info("AWS Secret Key: #{zkconfigsets_s3_aws_secret_access_key}")
+    #Chef::Log.info("AWS Secret Key: #{zkconfigsets_s3_aws_secret_access_key}")
     aws_secret_access_key new_resource.zkconfigsets_s3_aws_secret_access_key
   end
 
@@ -49,7 +49,7 @@ action :getconfig do
     EOF
   end
   
-  Chef::Log.info("Finishing deploy_custom_config recipe...")
+  #Chef::Log.info("Finishing deploy_custom_config recipe...")
   
   new_resource.updated_by_last_action(true)
 end
